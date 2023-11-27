1c6576528372648e953bc7dca6b19d631171eb17
Yeggnog <jacollins128@gmail.com>
2023-11-26 18:49:51 -0700

    Fixing overwrite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 process.env = Object.assign(process.env, loadEnv('production', process.cwd(), ''));
