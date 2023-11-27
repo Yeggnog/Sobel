381f187c2de5b313e852440d7d7900aefb796ecc
Yeggnog <jacollins128@gmail.com>
2023-11-26 18:48:41 -0700

    Overwriting vite url building

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+process.env = Object.assign(process.env, loadEnv('production', process.cwd(), ''));
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sobel/'
+  experimental: {
+    renderBuiltUrl(filename) {
+        return process.env.APP_URL + '/Sobel/' + filename;
+    }
+  }
 })
